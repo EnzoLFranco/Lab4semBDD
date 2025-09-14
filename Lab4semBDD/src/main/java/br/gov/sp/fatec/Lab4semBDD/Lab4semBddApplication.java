773630c31ef984faa44f80e7a0ad1feb8b66a94c
EnzoLFranco <fenzolemos@gmail.com>
2025-09-14 17:05:45 -0300

    Initial Commit

---
+++ Lab4semBDD/src/main/java/br/gov/sp/fatec/Lab4semBDD/Lab4semBddApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Lab4semBDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4semBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4semBddApplication.class, args);
+	}
+
+}
---
+++ Lab4semBDD/src/test/java/br/gov/sp/fatec/Lab4semBDD/Lab4semBddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Lab4semBDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4semBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
