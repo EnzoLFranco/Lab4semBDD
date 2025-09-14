d033e43d2e6e3eb11311f42a9ed741608d998b9b
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2025-09-14 21:59:12 +0000

    Cria serviço UsuarioService com método para buscar usuário por ID

---
+++ src/main/java/br/gov/sp/fatec/Lab4semBDD/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package br.gov.sp.fatec.Lab4semBDD.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import br.gov.sp.fatec.Lab4semBDD.entity.Usuario;
+import br.gov.sp.fatec.Lab4semBDD.repository.UsuarioRepository;
+
+@Service 
+public class UsuarioService {
+
+    @Autowired 
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario buscarPorId(Long id){
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if(usuarioOp.isPresent()) {
+            return usuarioOp.get();
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id inválido!");
+    }
+
+    
+
+}
