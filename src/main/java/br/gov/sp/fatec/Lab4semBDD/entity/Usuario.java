1709608ea136a870b65c81dd98df7eb7a8b3666e
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2025-09-14 20:51:46 +0000

    Adiciona métodos getters e setters à entidade Usuario

--- src/main/java/br/gov/sp/fatec/Lab4semBDD/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/Lab4semBDD/entity/Usuario.java
@@ -17,4 +17,36 @@ public class Usuario {
     @Column(name = "usr_senha")
     private String senha;
 
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String senha) {
+        this();
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
