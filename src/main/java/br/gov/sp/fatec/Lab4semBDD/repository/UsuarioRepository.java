2e2ebd803fb142bfca953f871edb189f183ea5a6
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2025-09-15 23:41:02 +0000

    Remove espaços em branco e reorganiza importações no UsuarioRepository

--- src/main/java/br/gov/sp/fatec/Lab4semBDD/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/Lab4semBDD/repository/UsuarioRepository.java
@@ -1,10 +1,10 @@
 package br.gov.sp.fatec.Lab4semBDD.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import br.gov.sp.fatec.Lab4semBDD.entity.Usuario;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
