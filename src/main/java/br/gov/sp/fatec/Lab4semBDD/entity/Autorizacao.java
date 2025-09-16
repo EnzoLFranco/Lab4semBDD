03543cdda90cf40bc4cdd93a985dd1ccb50330a8
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2025-09-16 21:42:41 +0000

    Cria a classe Autorizacao com mapeamento JPA e atributos para id e nome

---
+++ src/main/java/br/gov/sp/fatec/Lab4semBDD/entity/Autorizacao.java
@@ -0,0 +1,42 @@
+package br.gov.sp.fatec.Lab4semBDD.entity;
+
+import java.lang.annotation.Inherited;
+
+import javax.annotation.processing.Generated;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aut_id")
+    private Long id;
+
+    @Column(name = "aut_nome")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
