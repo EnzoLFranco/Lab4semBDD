d090f21622ced1d94f1edbc0f01203206e36c5d8
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2025-09-18 20:02:18 +0000

    Adiciona tabela tra_trabalho e insere dados iniciais

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -53,3 +53,19 @@ insert into ant_anotacao(ant_texto, ant_data_hora, ant_usr_id)
 -- cria usuário de aplicação
 create user spring with password 'pass123';
 grant update, delete, insert, select on all tables in schema public to spring;
+
+create table tra_trabalho (
+ tra_id bigint generated always as identity,
+ tra_titulo varchar(100) not null unique,
+ tra_data_hora_entrega timestamp not null,
+ tra_descricao varchar(200),
+ tra_usr_id bigint not null,
+ tra_nota int,
+ foreign key (tra_usr_id) references usr_usuario (usr_id) on delete restrict on update cascade
+);
+
+insert into tra_trabalho (tra_titulo, tra_data_hora_entrega, tra_nota, tra_usr_id)
+ values ('Teste 1', current_timestamp, 6, 1),
+ ('Teste 2', current_timestamp, null, 1);
+
+ GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE tra_trabalho TO spring;
